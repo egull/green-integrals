0be286b62e70ada4ff4e717b205bb885b9deeb29
Emanuel Gull <egull@users.noreply.github.com>
2024-12-17 10:17:29 +0100

    Update test_mpi.cpp

--- test/test_mpi.cpp
+++ test/test_mpi.cpp
@@ -8,8 +8,8 @@ TEST_CASE("Size", "[MPI]") {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  if(size==1){ std::cerr<<"MPI size is 1. most tests and possible problems will occur with MPI sizes >1"<<std::endl; return;}
+  if(size==1){ std::cerr<<"WARNING: MPI size is 1. most tests and possible problems will occur with MPI sizes >1"<<std::endl; return;}
 
-
-  REQUIRE(size>=2);  //if this throws run the tests with mpirun -np X, X>=2. 
+  // test disabled due to automatic checks running with MPI size 1.
+  // REQUIRE(size>=2);  //if this throws run the tests with mpirun -np X, X>=2. 
 }
