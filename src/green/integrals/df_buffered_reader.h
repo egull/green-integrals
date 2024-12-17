3ce13ca0456609a8aee0d5c0efaf1f824be9485f
Gaurav Harsha <gauravharsha05@gmail.com>
2024-12-17 17:02:38 -0500

    update throw statement for incorrect version of integrals

--- src/green/integrals/df_buffered_reader.h
+++ src/green/integrals/df_buffered_reader.h
@@ -41,14 +41,10 @@ namespace green::integrals {
       if(ar.has_attribute("__green_version__")) {
         std::string int_version = ar.get_attribute<std::string>("__green_version__");
         if (int_version.rfind(INPUT_VERSION, 0) != 0) {
-          //throw integrals_outdated_input("Integral files at '" + path +"' are outdated, please run migration script python/migrate.py");
-          std::cerr<<"Integral files at '" + path +"' are outdated, please run migration script python/migrate.py"<<std::endl;
-          std::cerr<<"we should really throw but we won't until the debug files have been updated."<<std::endl;
+          throw integrals_outdated_input("Integral files at '" + path +"' are outdated, please run migration script python/migrate.py");
         }
       } else {
-        //throw integrals_outdated_input("Integral files at '" + path +"' are outdated, please run migration script python/migrate.py");
-          std::cerr<<"Integral files at '" + path +"' are outdated, please run migration script python/migrate.py"<<std::endl;
-          std::cerr<<"we should really throw but we won't until the debug files have been updated."<<std::endl;
+        throw integrals_outdated_input("Integral files at '" + path +"' are outdated, please run migration script python/migrate.py");
       }
       ar.close();
 
